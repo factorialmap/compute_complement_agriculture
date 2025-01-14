ecbe0f38b38fe51edc4ed447b2530cc6c4635590
factorialmap <marcelo.carvalho.anjos@hotmail.com>
2025-01-14 12:33:12 -0300

    one and two sample t.test

--- two_sample_t_test.R
+++ two_sample_t_test.R
@@ -1,28 +1,55 @@
-#Is our yield different from the population yield?
-data_yield %>% 
-  t_test(response = yield,
-         mu = 1810)
+#load packages
+library(tidyverse)
+library(infer)
+library(ggpubr)
 
-#Is the mpg different between groups v and s?
-mtcars %>% 
-  select(vs, mpg) %>% 
-  mutate(vs = as.factor(vs)) %>% 
-  group_by(vs) %>% 
-  ungroup() %>% 
-  summarize(t_test = t.test(mpg~vs)$p.val)
+# data --------------------------------------------------------------------
+data_sbeet_yield <- 
+  read.csv("http://rstats4ag.org/data/sugarbeet.csv") %>% 
+  janitor::clean_names() %>% 
+  mutate(yield = round(yield*2.24,1),
+         type = as.factor(type))
 
-#how extract more info from this calculations?
-mtcars %>% 
-  summarize(ttest_mdl = list(t.test(mpg~vs))) %>% 
-  mutate(res_ttest = map(ttest_mdl, broom::tidy)) %>% 
-  unnest(res_ttest)
+# explore -----------------------------------------------------------------
+data_sbeet_yield %>% 
+  gghistogram(x = "yield",
+              add = "mean",
+              rug = "TRUE",
+              color = "type",
+              fill = "type")
 
+data_sbeet_yield %>% 
+  ggboxplot(x = "type",
+            y = "yield",
+            fill = "type",
+            add = "jitter")+
+  stat_compare_means()
 
-#how to plot this results
-mtcars %>% 
-  mutate(vs = as.factor(vs)) %>% 
-  ggplot(aes(x = vs, y= mpg, fill = vs))+
-  geom_boxplot(outlier.shape = NA)+
-  geom_jitter(alpha = 0.3)+
-  theme(legend.position = "none")
+# approach 1 --------------------------------------------------------------
+
+#calculate the difference between the groups
+sbeet_observed_stat <- 
+  data_sbeet_yield %>% 
+  specify(yield ~ type) %>% 
+  calculate(stat = "diff in means", 
+            order = c("R", "C")) #mean of R - mean of C
+
+#generate the null distribution with randomization
+null_dist_two_sample <- 
+  data_sbeet_yield %>% 
+  specify(yield~type) %>% 
+  hypothesize(null = "independence") %>% 
+  generate(reps = 1000, type = "permute") %>% 
+  calculate(stat = "diff in means", order = c("R","C"))
+
+#plot the results
+null_dist_two_sample %>% 
+  visualize() +
+  shade_p_value(sbeet_observed_stat,
+                direction = "two-sided")
+
+#calculate p-value
+null_dist_two_sample %>% 
+  get_p_value(obs_stat = sbeet_observed_stat,
+              direction = "two-sided")
 
